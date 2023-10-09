950c7f62fbf6d48af857d7c2a2e013fb77083e98
Jonathan Kirch <jonathankirch101@gmail.com>
2023-10-09 18:29:59 -0300

    melhorando teste de responsividade tag main

--- src/App.js
+++ src/App.js
@@ -10,10 +10,18 @@ function App() {
 			</header>
 
 			<main>
-				<Products nameProduct="Teste 1" imgProduct="https://via.placeholder.com/100&text=Sem imagem"/>
-				<Products nameProduct="Teste 2" imgProduct="https://via.placeholder.com/100&text=Sem imagem" />
+				<Products nameProduct="Teste 1" imgProduct="https://via.placeholder.com/170x150" />
+				<Products nameProduct="Teste 2" imgProduct="https://via.placeholder.com/1000" />
 				<Products nameProduct="Teste 3" imgProduct="https://via.placeholder.com/100&text=Sem imagem" />
 				<Products nameProduct="Teste 4" imgProduct="https://via.placeholder.com/100&text=Sem imagem" />
+				<Products nameProduct="Teste 5" imgProduct="https://via.placeholder.com/100&text=Sem imagem"/>
+				<Products nameProduct="Teste 6" imgProduct="https://via.placeholder.com/100&text=Sem imagem" />
+				<Products nameProduct="Teste 7" imgProduct="https://via.placeholder.com/100&text=Sem imagem" />
+				<Products nameProduct="Teste 8" imgProduct="https://via.placeholder.com/100&text=Sem imagem" />
+				<Products nameProduct="Teste 9" imgProduct="https://via.placeholder.com/100&text=Sem imagem"/>
+				<Products nameProduct="Teste 10" imgProduct="https://via.placeholder.com/100&text=Sem imagem" />
+				<Products nameProduct="Teste 11" imgProduct="https://via.placeholder.com/100&text=Sem imagem" />
+				<Products nameProduct="Teste 12" imgProduct="https://via.placeholder.com/100&text=Sem imagem" />
 			</main>
 
 			<section>Informações da Empresa</section>
