993fada1a55537849e9fee56d32d3f87690835b7
Jonathan Kirch <jonathankirch101@gmail.com>
2023-10-10 17:54:32 -0300

    tentando adicionar estilo

--- src/App.js
+++ src/App.js
@@ -15,47 +15,57 @@ function App() {
 			<Main />
 
 			<section className={styles.section}>
-				<h2>Sobre nossa empresa:</h2>
-				<p>
-					O Troca Fácil foi fundado em 2023 com a missão de tornar a
-					troca de produtos mais acessível e sustentável. Estamos
-					comprometidos em criar uma comunidade de troca confiável e
-					eficiente.
-				</p>
+				<div className={styles.divSection}>
+					<h2>Sobre nossa empresa:</h2>
+					<p>
+						O Troca Fácil foi fundado em 2023 com a missão de tornar
+						a troca de produtos mais acessível e sustentável.
+						Estamos comprometidos em criar uma comunidade de troca
+						confiável e eficiente.
+					</p>
 
-				<h3>Onde você pode nos encontrar?</h3>
-				<p>
-					Estamos localizados na &#8658;
-					<a href="https://maps.app.goo.gl/ottF5F1RnD26o68z5" target='_blank' rel='noopener noreferrer'>
-						Rodovia RS 453, Nr 15276, Teutônia - RS, 95890-000
-					</a>
-				</p>
+					<h3>Onde você pode nos encontrar?</h3>
+					<p>
+						Estamos localizados na &#8658;
+						<a
+							href="https://maps.app.goo.gl/ottF5F1RnD26o68z5"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Rodovia RS 453, Nr 15276, Teutônia - RS, 95890-000
+						</a>
+					</p>
+				</div>
 			</section>
 
 			<footer className={styles.rodape}>
 				<h3>Entre em Contato</h3>
 				<p>
-					Entre em contato pelo nosso <a href="mailto:jonathankirch101@gmail.com?subject=Olá, gostaria de entrar em contato para falar sobre a empresa Troca Fácil!">E-mail</a>, ou por nossas redes
-					socias:
+					Entre em contato pelo nosso{' '}
+					<a href="mailto:jonathankirch101@gmail.com?subject=Olá, gostaria de entrar em contato para falar sobre a empresa Troca Fácil!">
+						E-mail
+					</a>
+					, ou por nossas redes socias:
 				</p>
 				<div className={styles.icons}>
 					<ul>
 						<li>
 							<a href="/">
-								<FaFacebook color="black" />
-								@trocafacil
+								<FaFacebook size={30} color="#fb3640" />
 							</a>
 						</li>
 						<li>
-							<a href="https://instagram.com/alan_caye?igshid=MWZjMTM2ODFkZg==" target='_blank' rel='noopener noreferrer'>
-								<FaInstagram color="black" />
-								@trocafacil_oficial
+							<a
+								href="https://instagram.com/alan_caye?igshid=MWZjMTM2ODFkZg=="
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<FaInstagram size={30} color="#fb3640" />
 							</a>
 						</li>
 						<li>
 							<a href="https://api.whatsapp.com/send?phone=5551996449577&text=Ol%C3%A1%2C%20gostaria%20de%20entrar%20em%20contato%20para%20falar%20sobre%20sua%20empresa%20Troca%20F%C3%A1cil%21%0A%0AAssunto%3A%0A">
-								<FaWhatsapp color="black" />
-								Troca Fácil
+								<FaWhatsapp size={30} color="#fb3640" />
 							</a>
 						</li>
 					</ul>
